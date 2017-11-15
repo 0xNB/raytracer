2cab88ae4d2ef09b77ffa7d0a72498321f3be6a0
Einstein <ungep@student.kit.edu>
2017-11-16 00:24:05 +0100

    Added some logic

--- tracer.c
+++ tracer.c
@@ -33,7 +33,7 @@ struct Image{
 
 struct Image *img;
 
-// Needs to be freed from memory
+// Caller handles memory
 struct Vec * compute_direction(Vec start, Vec end){
     struct Vec *returnVec = malloc(sizeof(struct Vec));
     returnVec->x = end.x - start.x;
@@ -58,6 +58,39 @@ void render_image(){
            struct Vec *primary_dir = compute_direction(*cam, );
        }
     }
+}
+
+void vec_multiply(struct Vec *vec, double mult){
+      vec->x *= mult;
+      vec->y *= mult;
+      vec->z *= mult;
+}
+
+double vec_product(struct Vec vec1, struct Vec vec2){
+      return vec1.x * vec2.x + vec1.y * vec2.y + vec3.z * vec3.z;
+}
+
+//Caller handles memory
+struct Vec * vec_difference(struct Vec v2, struct Vec v1){
+	struct Vec *return_vec = malloc(sizeof(struct Vec));
+        return_vec->x = v2.x - v1.x;
+        return_vec->y = v2.y - v1.y;
+        return_vec->z = v2.z - v1.z;
+	return return_vec;
+}
+
+double vec_norm(struct Vec vec){
+         return sqrt(pow(vec.x,2) + pow(vec.y,2) + pow(vec.z,2));
+}
+
+double sphere_line_intersection(struct Sphere sphere, struct Vec vec, struct Vec origin){
+
+      // Calculate unit vector of vec
+      
+      double length = vec_norm(vec);
+      vec_multiply(&vec, 1/length);
+      struct Vec *diff_start_end = vec_difference(origin, sphere.center);
+      return pow(vec_product(start, *diff_start_end),2) - pow(vec_norm(diff_start_end),2) + pow(sphere.radius,2);
 }
 
 void free_everything(){
