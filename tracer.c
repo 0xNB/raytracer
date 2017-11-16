0bc04b55d725926b590e0e4c1f27635cc71dd283
Einstein <ungep@student.kit.edu>
2017-11-16 19:01:38 +0100

    Added some simple sphere tracing example

--- tracer.c
+++ tracer.c
@@ -2,15 +2,18 @@
 #include <math.h>
 #include <ncurses.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define NUM_SPHERES 2
+#define NUM_SPHERES 1
 #define RESOLUTION 0.02
 #define CAM_OFFSET 2
+#define MIN_DIST 5000
 
-const int max_x, max_y;
+int max_x, max_y;
 
 struct Vec{
    double x,y,z;
+   int color;
 };
 
 struct Line{
@@ -26,7 +29,7 @@ struct Sphere{
 struct Vec *cam;
 
 struct Image{
-    const int width, height;
+    int width, height;
     struct Vec **pixels;
     struct Sphere *spheres;
 };
@@ -34,7 +37,7 @@ struct Image{
 struct Image *img;
 
 // Caller handles memory
-struct Vec * compute_direction(Vec start, Vec end){
+struct Vec * compute_direction(struct Vec start,struct Vec end){
     struct Vec *returnVec = malloc(sizeof(struct Vec));
     returnVec->x = end.x - start.x;
     returnVec->y = end.y - start.y;
@@ -42,22 +45,22 @@ struct Vec * compute_direction(Vec start, Vec end){
     return returnVec;
 };
 
-void init_image(struct Image *img){
+void init_image(){
     for(int i = 0; i < img->width; i++){
         for(int j = 0; j < img->height; j++){
-            pixels[i][j].x = cam->x - (((double)img->width * RESOLUTION)/ 2 ) + i * RESOLUTION;
-	    pixels[i][j].y = cam->y - (((double)img->height * RESOLUTION)/ 2 ) + j * RESOLUTION;
-	    pixels[i][j].z = cam->z + CAM_OFFSET;
+            img->pixels[i][j].x = cam->x - (((double)img->width * RESOLUTION)/ 2 ) + i * RESOLUTION;
+	    img->pixels[i][j].y = cam->y - (((double)img->height * RESOLUTION)/ 2 ) + j * RESOLUTION;
+	    img->pixels[i][j].z = cam->z + CAM_OFFSET;
 	}
     }
 }
 
-void render_image(){
-    for(int i = 0; i < img->width; i++){
-       for(int j = 0; j < img->height; j++){
-           struct Vec *primary_dir = compute_direction(*cam, );
-       }
-    }
+struct Vec *create_vec(int x, int y, int z){
+     struct Vec *vec = malloc(sizeof(struct Vec));
+     vec->x = x;
+     vec->y = y;
+     vec->z = z;
+     return vec;
 }
 
 void vec_multiply(struct Vec *vec, double mult){
@@ -67,7 +70,7 @@ void vec_multiply(struct Vec *vec, double mult){
 }
 
 double vec_product(struct Vec vec1, struct Vec vec2){
-      return vec1.x * vec2.x + vec1.y * vec2.y + vec3.z * vec3.z;
+      return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
 }
 
 //Caller handles memory
@@ -89,14 +92,43 @@ double sphere_line_intersection(struct Sphere sphere, struct Vec vec, struct Vec
       
       double length = vec_norm(vec);
       vec_multiply(&vec, 1/length);
-      struct Vec *diff_start_end = vec_difference(origin, sphere.center);
-      return pow(vec_product(start, *diff_start_end),2) - pow(vec_norm(diff_start_end),2) + pow(sphere.radius,2);
+      struct Vec *diff_p = vec_difference(origin, sphere.center);
+      struct Vec diff_start_end = *diff_p;
+      double result = pow(vec_product(vec, diff_start_end),2) - pow(vec_norm(diff_start_end),2) + pow(sphere.radius,2); 
+      free(diff_p);
+      return result;
+}
+
+void render_image(){
+    clear();
+    for(int i = 0; i < img->width; i++){
+       for(int j = 0; j < img->height; j++){
+           struct Vec *primary_dir = compute_direction(*cam, img->pixels[i][j]);
+	   struct Vec p_hit;
+	   struct Vec normal_hit;
+	   double min_dist = MIN_DIST;
+
+           for(int k = 0; k < NUM_SPHERES; ++k){
+                 if(sphere_line_intersection(*(img->spheres + k), *primary_dir, *cam) >= 0){
+                     img->pixels[i][j].color = 1;
+		     mvprintw(j,i,".");
+		 }
+		 else{
+                     img->pixels[i][j].color = 0;
+		 }
+	   }
+	   free(primary_dir);
+       }
+    }
+    refresh();
+    usleep(500000);
 }
 
 void free_everything(){
     free(img->spheres);
     free(img->pixels);
     free(img);
+    free(cam);
 }
 
 int main(){
@@ -107,8 +139,24 @@ int main(){
          getmaxyx(stdscr, max_y, max_x);
 	 nodelay(stdscr, TRUE);
          img = malloc(sizeof(struct Image));
-	 img->pixels = malloc(max_x * max_y * sizeof(struct Vec));
 	 img->width = max_x;
 	 img->height = max_y;
+	 img->pixels = malloc(max_x * sizeof(*img->pixels));
+	 if(img->pixels){
+            for(int i = 0; i < max_x; i++){
+               img->pixels[i] = malloc(sizeof(*img->pixels[i]) * max_y);
+	    }
+	 }
 	 img->spheres = malloc(NUM_SPHERES * sizeof(struct Sphere));
+	 struct Vec *center_vec = create_vec(0,0,-5);
+	 img->spheres->center = *center_vec;
+	 img->spheres->radius = 5;
+	 cam = create_vec(0,0,-10); 
+	 init_image();
+	 for(int i = 0; i < 100; i ++){
+            img->spheres->center.z++;
+	    render_image();
+	 }
+	 free(center_vec);
+	 free_everything();
 }
